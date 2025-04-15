7972b98b5887d6557102023773e90a3b4adbcab4
Pierre-Cinart <cinartpierre@gmail.com>
2025-04-15 19:08:32 +0200

    ajout de plusieurs exemples html pour finaliser l aspect sur la maquette , revoir les surcharges css // ajout du fichier SQL pour la création de la base de données penser à faire un fichier caché pour les information de connexion

---
+++ database/ecoride.sql
@@ -0,0 +1,271 @@
+
+
+--
+-- Base de données : `ecoride`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `status` enum('pending','approved','refused') DEFAULT 'pending',
+  `submitted_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `preferences`
+--
+
+CREATE TABLE `preferences` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `accept_smokers` tinyint(1) DEFAULT '0',
+  `accept_animals` tinyint(1) DEFAULT '0',
+  `custom_preferences` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int NOT NULL,
+  `author_id` int NOT NULL,
+  `driver_id` int NOT NULL,
+  `rating` decimal(2,1) DEFAULT NULL,
+  `comment` text,
+  `status` enum('pending','accepted','refused') DEFAULT 'pending',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `trips`
+--
+
+CREATE TABLE `trips` (
+  `id` int NOT NULL,
+  `driver_id` int NOT NULL,
+  `vehicle_id` int NOT NULL,
+  `departure_city` varchar(100) DEFAULT NULL,
+  `arrival_city` varchar(100) DEFAULT NULL,
+  `departure_date` date DEFAULT NULL,
+  `departure_time` time DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `is_ecological` tinyint(1) DEFAULT '0',
+  `available_seats` int DEFAULT NULL,
+  `status` enum('planned','ongoing','completed','canceled') DEFAULT 'planned'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `trip_participants`
+--
+
+CREATE TABLE `trip_participants` (
+  `id` int NOT NULL,
+  `trip_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `confirmed` tinyint(1) DEFAULT '0',
+  `credits_used` int DEFAULT NULL,
+  `confirmation_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `role` enum('user','driver','employee','admin') NOT NULL DEFAULT 'user',
+  `credits` int DEFAULT '20',
+  `is_verified` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `jws_token` varchar(255) DEFAULT NULL,
+  `jws_token_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `model` varchar(100) DEFAULT NULL,
+  `color` varchar(50) DEFAULT NULL,
+  `fuel_type` enum('electric','hybrid','gasoline','diesel') DEFAULT NULL,
+  `registration_number` varchar(50) DEFAULT NULL,
+  `first_registration_date` date DEFAULT NULL,
+  `seats` int NOT NULL,
+  `picture` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_documents_user` (`user_id`);
+
+--
+-- Index pour la table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_preferences_user` (`user_id`);
+
+--
+-- Index pour la table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ratings_author` (`author_id`),
+  ADD KEY `fk_ratings_driver` (`driver_id`);
+
+--
+-- Index pour la table `trips`
+--
+ALTER TABLE `trips`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_trips_driver` (`driver_id`),
+  ADD KEY `fk_trips_vehicle` (`vehicle_id`);
+
+--
+-- Index pour la table `trip_participants`
+--
+ALTER TABLE `trip_participants`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_trip_participants_trip` (`trip_id`),
+  ADD KEY `fk_trip_participants_user` (`user_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_vehicles_user` (`user_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `preferences`
+--
+ALTER TABLE `preferences`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `trips`
+--
+ALTER TABLE `trips`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `trip_participants`
+--
+ALTER TABLE `trip_participants`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `fk_documents_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD CONSTRAINT `fk_preferences_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `fk_ratings_author` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `fk_ratings_driver` FOREIGN KEY (`driver_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `trips`
+--
+ALTER TABLE `trips`
+  ADD CONSTRAINT `fk_trips_driver` FOREIGN KEY (`driver_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `fk_trips_vehicle` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`);
+
+--
+-- Contraintes pour la table `trip_participants`
+--
+ALTER TABLE `trip_participants`
+  ADD CONSTRAINT `fk_trip_participants_trip` FOREIGN KEY (`trip_id`) REFERENCES `trips` (`id`),
+  ADD CONSTRAINT `fk_trip_participants_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD CONSTRAINT `fk_vehicles_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
